9bb24ae328f65dcca5d518c9a03f84ae62e0e0c2
kja9294 <40712026+kja9294@users.noreply.github.com>
2020-06-18 20:02:41 +0900

    Update try.php

--- try.php
+++ try.php
@@ -1,42 +1,25 @@
-<?php  
- 
-$link=mysqli_connect("localhost", "root", "비밀번호", "world");  
-if (!$link)
-{  
-    echo "MySQL 접속 에러 : ";
-    echo mysqli_connect_error();
-    exit();  
-}  
- 
-mysqli_set_charset($link,"utf8"); 
- 
- 
-$sql="select * from solar";
- 
-$result=mysqli_query($link,$sql);
-$data = array();   
-if($result){  
-    
-    while($row=mysqli_fetch_array($result)){
-        array_push($data, 
-            array('id'=>$row[0]
-        ));
-    }
- 
-    header('Content-Type: application/json; charset=utf8');
-$json = json_encode(array("webnautes"=>$data), JSON_PRETTY_PRINT+JSON_UNESCAPED_UNICODE);
-echo $json;
- 
-}  
-else{  
-    echo "SQL문 처리중 에러 발생 : "; 
-    echo mysqli_error($link);
-} 
- 
- 
- 
-mysqli_close($link);  
-   
+<?
+
+$a=3;
+
+$b=4;
+
+if($a==4 && $b==4){
+
+echo "a의 값은 4, b의 값은 4가 맞습니다.";
+
+}
+
+else if($a==4 || $b==4){
+
+echo "a와 b중 4가 하나는 있습니다.";
+
+}
+
+else{
+
+echo "해당사항은 없습니다.";
+
+}
+
 ?>
-
-
